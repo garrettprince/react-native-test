10749c3d6aae8d4c77ae1f7c6c91bd8020e5db26
garrettprince <garrettjamesprince@gmail.com>
2023-08-12 11:39:17 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Test</Text>
+      <Button title="Test" style={{ backgroundColor: "#000" }}></Button>
+      <TextInput
+        style={{ backgroundColor: "#000", color: "#fff", borderRadius: 5, padding: 10, }}
+      >
+        Hello
+      </TextInput>
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
